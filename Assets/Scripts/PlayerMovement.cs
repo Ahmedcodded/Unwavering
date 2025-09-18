1eb48d1dd9c8fab59b6fa40ecc19724e980faf24
GlitchyQuark <Ahmedfouad.essamohamed@gmail.com>
2025-09-18 22:44:58 +0300

    Added movement functionality and animations
    move, jump, crouch added, jump animation not finished

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,27 +1,56 @@
+using System;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerMovement : MonoBehaviour
 {
     public CharacterController2D controller;
+    public Animator animator;
 
     public float runSpeed = 40f;
 
     float HorizontalMove = 0f;
-    
+    bool jumpTrigger = false;
+    bool crouchTrigger = false;
+
+    InputAction moveAction;
+    InputAction jumpAction;
+    InputAction crouchAction;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-
+        moveAction = InputSystem.actions.FindAction("Move");
+        jumpAction = InputSystem.actions.FindAction("Jump");
+        crouchAction = InputSystem.actions.FindAction("Crouch");
     }
 
     // Update is called once per frame
     void Update()
     {
-        HorizontalMove = Input.GetAxisRaw("Horizontal") * runSpeed;
+        HorizontalMove = moveAction.ReadValue<Vector2>().x;
+        jumpTrigger = jumpAction.IsPressed();
+        crouchTrigger = crouchAction.IsPressed();
+
+        animator.SetFloat("Speed", Math.Abs(HorizontalMove));
+        if (jumpTrigger)
+        {
+            animator.SetBool("IsJumping", true);
+        }
     }
-    
+
     void FixedUpdate()
     {
-        controller.Move(HorizontalMove * Time.fixedDeltaTime, false, false);
+        controller.Move(HorizontalMove * Time.fixedDeltaTime * runSpeed, crouchTrigger, jumpTrigger);
+    }
+
+    public void OnLanding()
+    {
+        animator.SetBool("IsJumping", false);
+    }
+    
+    public void OnCrouching(bool isCrouching)
+    {
+        animator.SetBool("IsCrouching", isCrouching);
     }
 }
